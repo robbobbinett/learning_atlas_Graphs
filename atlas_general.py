173721a03a61af9f3c9f1c92e9b54cf0ce1dfb0f
robbobbinett <robinettryan8@gmail.com>
2025-09-15 14:35:09 -0500

    Writing code for distance experiment

--- atlas_general.py
+++ atlas_general.py
@@ -5,7 +5,9 @@ import itertools as it
 import json
 
 import numpy as np
-
+import networkx as nx
+from sklearn.metrics.pairwise import euclidean_distances
+from tqdm import tqdm
 
 from manifold_utils import LazyNeretinConvolution, quad_fit_full, \
 				get_quadratic_terms, \
@@ -38,9 +40,6 @@ class MemoryFIFOEnumeration:
 		# generate children
 		self.boundary_fun = boundary_fun
 
-		#DEBUG
-		self.b_val_log = []
-
 	def enqueue(self, obj):
 		if not (obj in self.visited):
 			self.candidates.append(obj)
@@ -49,7 +48,6 @@ class MemoryFIFOEnumeration:
 		candidate = self.candidates.pop(0)
 		cand_vec = np.array(candidate, dtype=int)
 		b_val = self.boundary_fun(self.eps * cand_vec)
-		self.b_val_log.append(b_val)
 		if b_val < 0:
 			for gen_vec in self.generators:
 				child_vec = cand_vec + gen_vec
@@ -60,11 +58,6 @@ class MemoryFIFOEnumeration:
 	def enumerate(self):
 		while len(self.candidates) > 0:
 			self.dequeue()
-			if len(self.visited) > 10000:
-#				for item in self.visited:
-				for item in self.b_val_log:
-					print(item)
-				raise ValueError
 
 class atlas_general:
 	def __init__(self, d, D):
@@ -112,12 +105,6 @@ class atlas_general:
 		b_0 = boundary_fun(np.zeros(self.d))
 		# Sometimes, origin of learned chart falls slightly
 		# out of boundary
-#		if b_0 > 0:
-#			c -= (b_0 + 1e-6)
-#			self.chart_dict[self.n_charts] = (x_0, L, M, h_mat,
-#							A, b, c)
-#			boundary_fun = self.construct_boundary_fun(x_0, L, M,
-#							h_mat, A, b, c)
 		self.boundary_fun_dict[self.n_charts] = boundary_fun
 		self.n_charts += 1
 
@@ -125,11 +112,6 @@ class atlas_general:
 		"""
 		Save self using pickle module
 		"""
-#		filepath = dirpath + "/" + filename
-#		if os.path.isfile(filepath):
-#			os.remove(filepath)
-#		with open(filepath, "w") as f:
-#			json.dump(self.chart_dict, f, indent=4)
 		os.system(f"rm -rf {dirpath}")
 		os.system(f"mkdir {dirpath}")
 		for j in range(self.n_charts):
@@ -159,7 +141,8 @@ class atlas_general:
 		# Return index with highest log-probability
 		return np.argmin(Logprobs, axis=1)
 
-	def xi_chart_to_ambient(self, xi, x_0, L, M, h_mat):
+	def xi_chart_to_ambient(self, xi, chart):
+		x_0, L, M, h_mat, _, _, _ = self.chart_dict[chart]
 		xi_quad = get_quadratic_and_const_terms(xi)
 		quad_term = M @ h_mat.T @ xi_quad
 		lin_term = L @ xi
@@ -173,13 +156,6 @@ class atlas_general:
 	def construct_boundary_fun(self, x_0, L, M, h_mat, A, b, c):
 		h_vec = h_mat[0, :]
 		H = h_mat[1:, :].T
-
-#		a = c + np.dot(x_0, A @ x_0) + np.dot(b, x_0)
-#		a_1 = 2 * L.T @ A.T @ x_0 + L.T @ b
-#		a_2 = H.T @ M.T @ A.T @ x_0 + H.T @ M.T @ b / 2
-#		A_2 = L.T @ A @ L
-#		A_3 = L.T @ A @ M @ H
-#		A_4 = H.T @ M.T @ A @ M @ H / 4
 
 		a = c + np.dot(x_0, A @ x_0) + np.dot(b, x_0)
 		a_1 = 2 * L.T @ A.T @ x_0 + L.T @ b
@@ -196,15 +172,12 @@ class atlas_general:
 			quad = np.dot(tau, A_2 @ tau)
 			quad_long = np.dot(a_2, tau_quad)
 			lin = np.dot(a_1, tau)
-#			return -(quart + cubic + quad + quad_long + lin + a)
 			return quart + cubic + quad + quad_long + lin + a
 		return boundary_fun
 
-#	def sample_uniformly_from_chart_by_ind(self, ind, n_pts, eps=0.1):
 	def sample_uniformly_from_chart_by_ind(self, ind, eps=0.1):
 		start = np.zeros(self.d)
 
-		x_0, L, M, h_mat, _, _, _ = self.chart_dict[ind]
 		boundary_fun = self.boundary_fun_dict[ind]
 		memory_fifo_enumerator = MemoryFIFOEnumeration(start, eps,
 							boundary_fun)
@@ -214,7 +187,7 @@ class atlas_general:
 		X_pre = []
 		for xi_pre in Xi_pre:
 			xi = eps * np.array(xi_pre)
-			x = self.xi_chart_to_ambient(xi, x_0, L, M, h_mat)
+			x = self.xi_chart_to_ambient(xi, ind)
 			X_pre.append(x)
 
 		return np.vstack(X_pre)
@@ -242,12 +215,10 @@ class atlas_general:
 		# Store losses
 		losses = []
 		for ind in range(self.n_charts):
-			x_0, L_p, M_p, h_mat, _, _, _, _ = self.chart_dict[ind]
+			x_0, L_p, M_p, h_mat, _, _, _ = self.chart_dict[ind]
 			loss = self.chart_to_fun(X, x_0, L_p, M_p, h_mat)
 			losses.append(loss[0])
-		print(np.max(losses))
-		print(np.argmax(losses))
-		print("\n")
+
 		return np.argmin(losses)
 
 	def ingest_ambient_point_given_ind(self, x, ind):
@@ -255,6 +226,83 @@ class atlas_general:
 		x_trans = x_0 - x
 		xi = L_p.T @ x_trans
 		return xi
+
+	def construct_dense_graph(self, delta=0.1, eps=1.0):
+		"""Construct a fine mesh of the manifold by sampling from each
+		coordinate chart. Edge weights determined by ambient distance.
+		This graph is used to compute graph shortest-paths between points,
+		which are used as a proxy for geodesic paths and path-lengths."""
+		# Get points for graph vertices
+		X_pre = []
+		for chart in tqdm(range(self.n_charts)):
+			x_0, L, M, h_mat, A, b, c = self.chart_dict[chart]
+			boundary_fun = self.boundary_fun_dict[chart]
+			X_smol = self.sample_uniformly_from_chart_by_ind(chart,
+						eps=delta)
+			X_pre.append(X_smol)
+		X = np.vstack(X_pre)
+
+		# Generate graph
+		n_brute = X.shape[0]
+		G = nx.Graph()
+		for j in tqdm(range(n_brute)):
+			X_j = X[j, :].reshape(1, 9)
+			G.add_node(j)
+			dist_vec = euclidean_distances(X_j, X)[0, :]
+			for k in range(j+1, n_brute):
+				dist = dist_vec[k]
+				if dist <= eps:
+					G.add_edge(j, k, weight=dist)
+
+		self.X_brute = X
+		self.G_brute = G
+		# Cache shortest paths between points in self.X_brute
+		self.shortest_path_cache = {}
+
+	def shortest_path_cached(self, ind_0, ind_1):
+		"""Return shortest path as list of nodes; memoized to improve
+		performance. Must run construct_dense_graph method before calling
+		this method."""
+		key = (ind_0, ind_1)
+		try:
+			return self.shortest_path_cache[key]
+		except KeyError:
+			path = nx.shortest_path(self.G_brute, weight="weight",
+					source=ind_0, target=ind_1)
+			path_len = nx.shortest_path_length(self.G_brute,
+					weight="weight", source=ind_0,
+					target=ind_1)
+			self.shortest_path_cache[key] = (path, path_len)
+
+			return path, path_len
+
+	def find_closest_brute_point(self, x):
+		"""Find the closest point to x in self.X_brute. This method
+		should only be called after calling the method
+		construct_dense_graph """
+		X = x.reshape(1, 9)
+		dists = euclidean_distances(X, self.X_brute)[0, :]
+		return np.argmin(dists)
+
+	def approximate_shortest_path(self, xi_0, chart_0, xi_1, chart_1):
+		"""Graph-shortest-path between two points in the atlas-graph.
+		Make sure that dense subgraph has already been sampled before
+		calling this method."""
+		x_0 = self.xi_chart_to_ambient(xi_0, chart_0)
+		x_1 = self.xi_chart_to_ambient(xi_1, chart_1)
+		ind_0 = self.find_closest_brute_point(x_0)
+		ind_1 = self.find_closest_brute_point(x_1)
+		x_prime_0 = self.X_brute[ind_0, :]
+		x_prime_1 = self.X_brute[ind_1, :]
+		dist_0 = np.linalg.norm(x_0 - x_prime_0)
+		dist_1 = np.linalg.norm(x_1 - x_prime_1)
+		try:
+			path_pre, path_len = self.shortest_path_cached(ind_0, ind_1)
+		except AttributeError:
+			raise AttributeError("Must run construct_dense_graph method before calling this method.")
+		path = path_pre.copy()
+		return dist_0, dist_1, path, path_len
+
 
 	def ingest_ambient_point(self, x):
 		# Identify best chart
